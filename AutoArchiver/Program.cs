c2a7b9da78e38d49ae8ce3ee6228c5eb521c1821
PMG <PMG@DESKTOP-E9QQU0V>
2024-06-03 14:32:14 +0300

    commit

--- AutoArchiver/Program.cs
+++ AutoArchiver/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace AutoArchiver
+﻿using Aspose.Zip;
+using System.IO.Compression;
+
+namespace AutoArchiver
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                string path = @"C:\Users\PMG.DESKTOP-E9QQU0V\Desktop\Test";
+
+                using (FileStream zipFile = File.Open(path, FileMode.Create))
+                {
+                    using (var archive = new Archive())
+                    {
+                        foreach (var entry in archive.Entries)
+                        {
+                            archive.CreateEntry(entry.Name, zipFile);
+                        }
+                        archive.Save(zipFile);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Oops. I don't have acces to this file.");
+            }
+            catch (Exception)
+            {
+            
+            }
+
+            
+            
         }
     }
 }
